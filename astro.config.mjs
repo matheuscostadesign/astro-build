1fce4b3371acdd07b22526c97cba7f6031c1a821
Matheus Costa <matheuscostadosantos@hotmail.com>
2023-02-15 15:48:01 -0300

    refactor(lp): Melhorias gerais projeto

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  integrations: [tailwind(), image(), mdx()]
 });
