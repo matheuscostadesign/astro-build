e9b401951cc352aec8bddfe407c39a7b5bc1ee0c
Matheus Costa <matheuscostadosantos@hotmail.com>
2023-02-16 16:42:59 -0300

    feat(lp): adiciona lib astro-prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,24 @@
 import { defineConfig } from "astro/config";
 
+// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 import image from "@astrojs/image";
 
+// https://astro.build/config
 import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 import compress from "astro-compress";
 
+// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
 export default defineConfig({
-  site: "https://astrobuild-lp.vercel.app/", // necessário p/ sitemap
-  integrations: [tailwind(), image(), mdx(), compress(), sitemap()],
+  integrations: [tailwind(), image(), mdx(), compress(), sitemap(), prefetch()]
 });
